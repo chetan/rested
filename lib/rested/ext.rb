f7fe5496ace8e8c6a22a730e78fb711ea9032d6e
Alex Catighera <acatighera@gmail.com>
2012-03-13 14:40:42 -0400

    fix for ruby 1.9.3-p125

--- lib/rested/ext.rb
+++ lib/rested/ext.rb
@@ -1,16 +1,18 @@
 
 # orginally cribbed from Rails edge. modified a bunch.
 
-class Hash
-  # By default, only instances of Hash itself are extractable.
-  # Subclasses of Hash may implement this method and return
-  # true to declare themselves as extractable. If a Hash
-  # is extractable, Array#extract_options! pops it from
-  # the Array when it is the last element of the Array.
-  def extractable_options?
-    instance_of?(Hash)
-  end
-end if not {}.respond_to? :extractable_options?
+if !({}).respond_to?(:extractable_options?)
+  class Hash
+    # By default, only instances of Hash itself are extractable.
+    # Subclasses of Hash may implement this method and return
+    # true to declare themselves as extractable. If a Hash
+    # is extractable, Array#extract_options! pops it from
+    # the Array when it is the last element of the Array.
+    def extractable_options?
+      instance_of?(Hash)
+    end
+  end 
+end
 
 class Array
   # Extracts options from a set of arguments. Removes and returns the last
